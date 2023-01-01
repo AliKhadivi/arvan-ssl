9cb18ac3f608558506453731f008dc208f208839
khadivi <javad.khadivi@gmail.com>
2023-01-01 18:24:25 +0330

    Fixed

--- authenticator.sh
+++ authenticator.sh
@@ -2,7 +2,7 @@
 PROG_DIR="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
 source "${PROG_DIR}/.env" || exit 1
 
-echo "ADD TXT: $certbot_VALIDATION"
+echo "ADD TXT: $CERTBOT_VALIDATION"
 
 # Create TXT record
 CREATE_DOMAIN="_acme-challenge" # For Arvan cloud
@@ -10,7 +10,7 @@ CREATE_DOMAIN="_acme-challenge" # For Arvan cloud
 RECORD_ID=$(curl -s -X POST "https://napi.arvancloud.ir/cdn/4.0/domains/$CERTBOT_DOMAIN/dns-records" \
      -H     "Authorization: $API_KEY" \
      -H     "Content-Type: application/json" \
-     --data '{"type":"TXT","name":"'"$CREATE_DOMAIN"'","value":{"text": "'"$certbot_VALIDATION"'"},"ttl":120}' \
+     --data '{"type":"TXT","name":"'"$CREATE_DOMAIN"'","value":{"text": "'"$CERTBOT_VALIDATION"'"},"ttl":120}' \
              | python -c "import sys,json;print(json.load(sys.stdin)['data']['id'])")
              
 # Save info for cleanup
@@ -20,4 +20,5 @@ fi
 echo $RECORD_ID >> /tmp/certbot_$CERTBOT_DOMAIN/RECORD_IDS
 
 # Sleep to make sure the change has time to propagate over to DNS
+echo "Wait ${delay:-45} seconds to apply."
 sleep ${delay:-45}
