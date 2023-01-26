a5c9d35b9f72256b92322dcc3d6b50740b5e3ea6
Ali Khadivi <74895885+AliKhadivi@users.noreply.github.com>
2023-01-26 12:16:11 +0330

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,25 @@
-# #!/bin/bash
-# PROG_DIR="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
-# source "${PROG_DIR}/.env" || exit 1
+#!/bin/bash
+PROG_DIR="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
+source "${PROG_DIR}/.env" || exit 1
 
+if [ -x "$(command -v haproxy)" ]; then
+    echo "Genrate HAProxy fullchain for $CERTBOT_DOMAIN"
+    cat /etc/letsencrypt/live/$CERTBOT_DOMAIN/fullchain.pem /etc/letsencrypt/live/$CERTBOT_DOMAIN/privkey.pem > /etc/haproxy/ssl/$CERTBOT_DOMAIN.pem
 
-# echo "Genrate HAProxy fullchain for $CERTBOT_DOMAIN"
-# cat /etc/letsencrypt/live/$CERTBOT_DOMAIN/fullchain.pem /etc/letsencrypt/live/$CERTBOT_DOMAIN/privkey.pem > /etc/haproxy/ssl/$CERTBOT_DOMAIN.pem
-
-# echo "Reload HAProxy"
-# systemctl reload haproxy
-
-# echo "Upload certificate to Arvan"
-# curl -L --request POST "https://napi.arvancloud.com/cdn/4.0/domains/$CERTBOT_DOMAIN/https/certificate" \
-# --header "Authorization: $API_KEY" \
-# --form "f_ssl_user_key=@$(realpath /etc/letsencrypt/live/$CERTBOT_DOMAIN/privkey.pem)" \
-# --form "f_ssl_user_cert=@$(realpath /etc/letsencrypt/live/$CERTBOT_DOMAIN/fullchain.pem)"
+    echo "Reload HAProxy"
+    systemctl reload haproxy
+fi
+if [ -x "$(command -v nginx)" ]; then
+    if nginx_out=$(nginx -t 2>&1); then
+        echo "Reloading Nginx..."
+        systemctl reload nginx
+        echo "Nginx reloaded!"
+    else
+        echo "Nginx configuration validation failed!"
+        echo "Detail:"
+        echo "$nginx_out"
+    fi
+fi
 
 
 
