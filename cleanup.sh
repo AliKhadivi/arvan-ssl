86455d9f4473ca2f43d653c5010dbf26b1cd0cf1
Ali Khadivi <74895885+AliKhadivi@users.noreply.github.com>
2023-05-27 19:02:24 +0330

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -8,12 +8,18 @@ if [ -f /tmp/certbot_$CERTBOT_DOMAIN/RECORD_IDS ]; then
         rm -rf /tmp/certbot_$CERTBOT_DOMAIN/
 fi
 
+if [ "$(echo "$CERTBOT_DOMAIN" | cut -d "." -f 3)" == "" ] ; then
+        DOMAIN="$CERTBOT_DOMAIN"
+else
+        DOMAIN="$(echo "$CERTBOT_DOMAIN" | cut -d "." -f 2).$(echo "$CERTBOT_DOMAIN" | cut -d "." -f 3)"
+fi
+
 # Remove the challenge TXT record from the zone
 if [ -n "${RECORD_IDS}" ]; then
     for RECORD_ID in $RECORD_IDS
     do 
         echo "Remove TXT record from $CERTBOT_DOMAIN: $RECORD_ID"
-        curl -s -X DELETE "https://napi.arvancloud.ir/cdn/4.0/domains/$CERTBOT_DOMAIN/dns-records/$RECORD_ID" \
+        curl -s -X DELETE "https://napi.arvancloud.ir/cdn/4.0/domains/$DOMAIN/dns-records/$RECORD_ID" \
                 -H "Authorization: $API_KEY"
         echo
     done
